43fb5e1ac2ba5d114b0ac7aa0043cb762eee9ed8
MJ <code.za@proton.me>
2023-04-06 13:31:32 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,18 @@
-import React, { Component } from 'react';
+import { Route, Routes } from "react-router-dom";
 
-class App extends Component {
-  state = {
-    courses: [],
-  };
+// App Components
+import Header from "./components/Header";
 
-  componentDidMount() {
-    this.fetchCourses();
-  }
+function App() {
 
-  /* Simple fetch for our Courses GET route from the Express server 
-  - for testing purposes only to ensure CORS etc correctly setup*/
+  return (
+    <div>
+      <Header />
+      {/* <Routes>
 
-  async fetchCourses() {
-    try {
-      const response = await fetch('http://localhost:5000/api/courses');
-      const courses = await response.json();
-      this.setState({ courses });
-    } catch (error) {
-      console.error('Error fetching courses:', error);
-    }
-  }
-
-  render() {
-    const { courses } = this.state;
-    return (
-      <div className="App">
-        <h1>Courses</h1>
-        <ul>
-          {courses.map((course) => (
-            <li key={course.id}>{course.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+      </Routes> */}
+    </div>
+  );
 }
 
 export default App;
