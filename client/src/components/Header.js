0de64a073a1f5d1499015f7fdff55d12fe956cf1
MJ <code.za@proton.me>
2023-04-06 13:45:05 +0200

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
     <header>
       <div className="wrap header--flex">
         <h1 className="header--logo">
-          <a href="index.html">Courses</a>
+          <a href="/">Courses</a>
         </h1>
         <nav>
           <ul className="header--signedout">
             <li>
-              <a href="sign-up.html">Sign Up</a>
+              <a href="/">Sign Up</a>
             </li>
             <li>
-              <a href="sign-in.html">Sign In</a>
+              <a href="/">Sign In</a>
             </li>
           </ul>
         </nav>
