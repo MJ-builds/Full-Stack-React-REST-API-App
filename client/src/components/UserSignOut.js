396cf8a36252914c06111327702256754172e245
MJ <code.za@proton.me>
2023-04-12 12:35:36 +0200

    Added simple signout nav to home

---
+++ client/src/components/UserSignOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Navigate } from 'react-router-dom';
+
+const UserSignOut = () => {
+  return (
+    <Navigate to="/" />
+  );
+}
+
+export default UserSignOut;
