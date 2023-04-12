5a944ca952110e69f48a2d4fea1344906424bbac
MJ <code.za@proton.me>
2023-04-12 13:16:26 +0200

    Added signup functionality p1

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,26 +1,115 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const UserSignUp = () => {
   const navigate = useNavigate();  
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const handleCancelClick = (event) => {
     event.preventDefault();
     navigate('/');
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let errors = [];
+
+    if (!firstName) {
+      errors.push("Please provide a value for 'First Name'");
+    }
+
+    if (!lastName) {
+      errors.push("Please provide a value for 'Last Name'");
+    }
+
+    if (!emailAddress) {
+      errors.push("Please provide a value for 'Email Address'");
+    }
+
+    if (!lastName) {
+      errors.push("Please provide a value for 'Password'");
+    }
+
+    if (errors.length > 0) {
+      setErrors(errors);
+      return;
+    }
+
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/users",
+          {
+            firstName: firstName,
+            lastName: lastName,
+            emailAddress: emailAddress,
+            password: password,
+          },
+        );
+
+        if (response.status === 201) {
+          navigate('/');
+        } else {
+          console.error("An error occurred while creating the course");
+        }
+      
+      } catch (error) {
+        console.error("An error occurred while creating the course", error);
+      }
+  };
+
   return (
     <div className="form--centered">
       <h2>Sign Up</h2>
-      <form>
+      {errors.length > 0 && (
+        <div className="validation--errors">
+          <h3>Validation Errors</h3>
+          <ul>
+            {errors.map((error, index) => (
+              <li key={index}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input id="firstName" name="firstName" type="text" defaultValue="" />
+        <input 
+        id="firstName" 
+        name="firstName" 
+        type="text" 
+        value={firstName} 
+        onChange={(e) => setFirstName(e.target.value)}  
+        />
         <label htmlFor="lastName">Last Name</label>
-        <input id="lastName" name="lastName" type="text" defaultValue="" />
+        <input 
+        id="lastName" 
+        name="lastName" 
+        type="text" 
+        value={lastName} 
+        onChange={(e) => setLastName(e.target.value)}
+        />
         <label htmlFor="emailAddress">Email Address</label>
-        <input id="emailAddress" name="emailAddress" type="email" defaultValue="" />
+        <input 
+        id="emailAddress" 
+        name="emailAddress" 
+        type="email" 
+        value={emailAddress} 
+        onChange={(e) => setEmailAddress(e.target.value)}  
+        />
         <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" defaultValue="" />
+        <input 
+        id="password" 
+        name="password" 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)}  
+        />
         <button className="button" type="submit">
           Sign Up
         </button>
