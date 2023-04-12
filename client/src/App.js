3e0d472b5bdc469e14e700ac4d243ce7dd06202e
MJ <code.za@proton.me>
2023-04-12 12:34:49 +0200

    Added PrivateRoute (auth. only)

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // App Components
 import Header from "./components/Header";
 import Courses from "./components/Courses";
-import CourseDetail from './components/CourseDetail';
-import UserSignIn from './components/UserSignIn';
-import UserSignUp from './components/UserSignUp';
-import CreateCourse from './components/CreateCourse';
-import UpdateCourse from './components/UpdateCourse';
+import CourseDetail from "./components/CourseDetail";
+import UserSignIn from "./components/UserSignIn";
+import UserSignUp from "./components/UserSignUp";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
+import UserSignOut from "./components/UserSignOut";
+import PrivateRoute from "./components/PrivateRoute";
 
-//TODO: Add Routes and PrivateRoute components
 function App() {
   return (
     <Router>
       <Header />
-          <Routes>
-            <Route path="/" element={<Courses />} />
-            <Route path="/courses/:courseId" element={<CourseDetail />} />
-            <Route path = "/signin" element = {<UserSignIn />} />
-            <Route path = "/signup" element = {<UserSignUp />} />
-            <Route path = "/courses/create" element = {<CreateCourse />} />
-            <Route path = "/courses/:courseId/update" element = {<UpdateCourse />} />
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/courses/:courseId" element={<CourseDetail />} />
+        <Route path="/signin" element={<UserSignIn />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/signout" element={<UserSignOut />} />
+        <Route path="/courses/create" element={<PrivateRoute />}>
+          <Route index element={<CreateCourse />} />
+        </Route>
+        <Route path="/courses/:courseId/update" element={<PrivateRoute />}>
+          <Route index element={<UpdateCourse />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
