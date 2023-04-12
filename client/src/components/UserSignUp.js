1a7ed8b2245147d16e99479fb86f8cddceb61c53
MJ <code.za@proton.me>
2023-04-12 15:34:13 +0200

    Update UserSignUp.js

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
-import {useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 
-import { useUserContext } from '../context/UserContext';
+import { useUserContext } from "../context/UserContext";
 
 const UserSignUp = () => {
-  const navigate = useNavigate();  
-
+  const navigate = useNavigate();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
@@ -15,32 +14,28 @@ const UserSignUp = () => {
 
   const { signIn } = useUserContext();
 
-
   const handleCancelClick = (event) => {
     event.preventDefault();
-    navigate('/');
+    navigate("/");
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
-      const response = await axios.post(
-        "http://localhost:5000/api/users",
-        {
-          firstName: firstName,
-          lastName: lastName,
-          emailAddress: emailAddress,
-          password: password,
-        },
-      );
-  
+      const response = await axios.post("http://localhost:5000/api/users", {
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: emailAddress,
+        password: password,
+      });
+
       if (response.status === 201) {
         // Call signIn function
         const signInSuccess = await signIn(emailAddress, password);
-  
+
         if (signInSuccess) {
-          navigate('/');
+          navigate("/");
         } else {
           console.error("An error occurred while signing in the new user");
         }
@@ -56,7 +51,7 @@ const UserSignUp = () => {
       }
     }
   };
-  
+
 
   return (
     <div className="form--centered">
@@ -73,36 +68,36 @@ const UserSignUp = () => {
       )}
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input 
-        id="firstName" 
-        name="firstName" 
-        type="text" 
-        value={firstName} 
-        onChange={(e) => setFirstName(e.target.value)}  
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
         <label htmlFor="lastName">Last Name</label>
-        <input 
-        id="lastName" 
-        name="lastName" 
-        type="text" 
-        value={lastName} 
-        onChange={(e) => setLastName(e.target.value)}
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
         />
         <label htmlFor="emailAddress">Email Address</label>
-        <input 
-        id="emailAddress" 
-        name="emailAddress" 
-        type="email" 
-        value={emailAddress} 
-        onChange={(e) => setEmailAddress(e.target.value)}  
+        <input
+          id="emailAddress"
+          name="emailAddress"
+          type="email"
+          value={emailAddress}
+          onChange={(e) => setEmailAddress(e.target.value)}
         />
         <label htmlFor="password">Password</label>
-        <input 
-        id="password" 
-        name="password" 
-        type="password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)}  
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="button" type="submit">
           Sign Up
@@ -112,7 +107,7 @@ const UserSignUp = () => {
         </button>
       </form>
       <p>
-        Already have a user account? Click here to{' '}
+        Already have a user account? Click here to{" "}
         <Link to="/signin  ">sign in</Link>!
       </p>
     </div>
