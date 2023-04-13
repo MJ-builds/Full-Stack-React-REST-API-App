b7b0b43b68fe42950d28d30f0c4e3a650dfc99d5
MJ <code.za@proton.me>
2023-04-13 23:12:15 +0200

    formatting

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,10 @@
 import { useUserContext } from "../context/UserContext";
 import { Link } from "react-router-dom";
 
-
 /* Header component that displays the header of the app, and depending on whether a user 
 is signed in or not, displays the appropriate navigation links 
 (First Name Last Name / Sign Out or sign In / Sign Out). */
 const Header = () => {
-
   const { authenticatedUser, signOut } = useUserContext();
 
   return (
@@ -16,26 +14,31 @@ const Header = () => {
           <Link to="/">Courses</Link>
         </h1>
         <nav>
-        {authenticatedUser ? (
-          <ul className="header--signedin">
-            <li>
-              Welcome, <div className= 'logged--in--user'>{authenticatedUser.firstName} {authenticatedUser.lastName}</div>!
-            </li>
-            <li>
-              <Link to="/signout" onClick={signOut}>Sign Out</Link>
-            </li>
-          </ul>
-        ) : (
-          <ul className="header--signedout">
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-          </ul>
-          )
-        }
+          {authenticatedUser ? (
+            <ul className="header--signedin">
+              <li>
+                Welcome,{" "}
+                <div className="logged--in--user">
+                  {authenticatedUser.firstName} {authenticatedUser.lastName}
+                </div>
+                !
+              </li>
+              <li>
+                <Link to="/signout" onClick={signOut}>
+                  Sign Out
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="header--signedout">
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign In</Link>
+              </li>
+            </ul>
+          )}
         </nav>
       </div>
     </header>
