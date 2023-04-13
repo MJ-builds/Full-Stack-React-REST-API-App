469a2577fcbc133f96d89dc54c229fa112001182
MJ <code.za@proton.me>
2023-04-13 22:07:07 +0200

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -103,8 +103,8 @@ const handleDeleteCourse = async () => {
             <div>
               <h3 className="course--detail--title">Estimated Time</h3>
               <p>{course.estimatedTime}</p>
-              <h3 className="course--detail--title ">Materials Needed</h3>
-                <ReactMarkdown className="course--detail--list">{course.materialsNeeded}</ReactMarkdown>
+              <h3 className="course--detail--title">Materials Needed</h3>
+                <ReactMarkdown className="course--detail--list materials-list-container course--detail--markdown">{course.materialsNeeded}</ReactMarkdown>
             </div>
           </div>
         </form>
