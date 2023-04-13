e1009ea2a823ee33a44ab79e17e3ea2f6974d9e3
MJ <code.za@proton.me>
2023-04-13 12:16:57 +0200

    slight rollback - unexpected errors

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,23 +1,9 @@
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
-// base url for all API requests
+//to flesh out later on - trying to include a global 500 server error handler here but may be the wrong place.
+
 const apiClient = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
-
-// intercept all responses and check for 500 status code (as part of Exceeds Expectations for this project)
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const navigate = useNavigate();
-
-    if (error.response && error.response.status === 500) {
-      navigate('/error');
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default apiClient;
