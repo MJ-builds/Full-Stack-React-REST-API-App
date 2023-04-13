8d50c745baa8255b36b07a77392e74edf8f21a0c
MJ <code.za@proton.me>
2023-04-13 23:21:01 +0200

    comments and formatting

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 //to flesh out later on - trying to include a global 500 server error handler here but may be the wrong place.
 
+// for now, this component simply provides a base URL for the API
 const apiClient = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
