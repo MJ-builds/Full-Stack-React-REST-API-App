308889e7813048e13bcd1186c67beb33113a15b9
MJ <code.za@proton.me>
2023-04-14 11:33:15 +0200

    Update apiClient.js

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 //to flesh out later on - trying to include a global 500 server error handler here but may be the wrong place.
 
 // for now, this component simply provides a base URL for the API
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "http://localhost:5000/api",
 });
 
 export default apiClient;
