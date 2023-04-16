63ce3a91bb5fe601ea1f13488bc6e98f6451186f
MJ <code.za@proton.me>
2023-04-16 15:32:18 +0200

    Update API Address for Deploy
    Change to "http://localhost:5000/api" for testing on your local machine

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -3,8 +3,10 @@ import axios from "axios";
 //to flesh out later on - trying to include a global 500 server error handler here but may be the wrong place.
 
 // for now, this component simply provides a base URL for the API
+
+//NB: CHANGE BELOW ADDRESS TO http://localhost:5000/api IF YOU ARE TESTING OUT ON YOUR LOCAL MACHINE
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://full-stack-react-rest-api-app-production.up.railway.app/",
 });
 
 export default apiClient;
