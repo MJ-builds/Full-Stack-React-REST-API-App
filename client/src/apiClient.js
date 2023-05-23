50c5a0ff43a373e8e4c3b0fa0a3c94e93504b3e2
Michael <112978328+MJ-builds@users.noreply.github.com>
2023-05-23 13:13:47 +0200

    Update apiClient.js

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 //NB: CHANGE BELOW ADDRESS TO http://localhost:5000/api IF YOU ARE TESTING OUT ON YOUR LOCAL MACHINE
 const apiClient = axios.create({
-  baseURL: "full-stack-react-rest-api-app.vercel.app",
+  baseURL: "http://localhost:5000/api",
 });
 
 export default apiClient;
